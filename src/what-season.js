65c93bb1bfce25d63b8199b4b478bf35ed7219ad
PrikhodkoAnna <prikhodkoannav@gmail.com>
2021-09-09 23:44:43 +0300

    feat: add solution of tower-of-hanoi

--- src/what-season.js
+++ src/what-season.js
@@ -16,60 +16,34 @@ export default
 
 function getSeason(date) {
 
- if(!date) {
-    return 'Unable to determine the time of year!';
-  } 
-  
-
- if ((Object.prototype.toString.call(date) !== '[object Date]') || (Object.keys(date).length !== 0)) {
-    throw new Error ('Invalid date!');
-
-  }
-  
-let month = date.getMonth();
-if (((month === 0) || (month === 1)) || (month === 11)){
-  return 'winter';
-}
-if ((month >= 2) && (month <= 4)) {
-  return 'spring';
-}
-if ((month >= 5) && (month <= 7)) {
-  return 'summer';
-}
-if ((month >= 8) && (month <= 10)) {
-  return 'fall';
-}
-  
-
-}
-
-
-/*const fakeDate = {
-  toString() {
-      return Date.prototype.toString.call(new Date());
-  },
-  [Symbol.toStringTag]: 'Date'
-};
-
-Object.setPrototypeOf(fakeDate, Object.getPrototypeOf(new Date()));
-
-getSeason(fakeDate);
-
-function getSeason(date) {
-
   if(!date) {
-    return 'Unable to determine the time of year!';
-  }
-
-  if ((Object.prototype.toString.call(date) === '[object Date]') && (!(isNaN(date.getTime())))) {
+     return 'Unable to determine the time of year!';
+   } 
+ 
+ 
+  if ((Object.prototype.toString.call(date) !== '[object Date]') || (Object.keys(date).length !== 0)) {
+     throw new Error ('Invalid date!');
+ 
+   }
+ 
+ let month = date.getMonth();
+ if (((month === 0) || (month === 1)) || (month === 11)){
+   return 'winter';
+ }
+ if ((month >= 2) && (month <= 4)) {
+   return 'spring';
+ }
+ if ((month >= 5) && (month <= 7)) {
+   return 'summer';
+ }
+ if ((month >= 8) && (month <= 10)) {
+   return 'fall';
+ }
+ 
+ 
+ }
   
-console.log(date.getTime());
-  }
 
-else {
-  
-throw new Error ('Invalid date!');
 
-}
 
-}*/
+
