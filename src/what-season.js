78b4a817383a2178529e1e8d39b74ffc96fdbbb4
PrikhodkoAnna <prikhodkoannav@gmail.com>
2021-09-09 13:33:13 +0300

    feat: add solution of what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,65 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default 
+/*getSeason('foo');*/
+
+function getSeason(date) {
+
+ if(!date) {
+    return 'Unable to determine the time of year!';
+  } 
+  
+
+ if ((Object.prototype.toString.call(date) !== '[object Date]') || (Object.keys(date).length !== 0)) {
+    throw new Error ('Invalid date!');
+
+  }
+  
+let month = date.getMonth();
+if (((month === 0) || (month === 1)) || (month === 11)){
+  return 'winter';
 }
+if ((month >= 2) && (month <= 4)) {
+  return 'spring';
+}
+if ((month >= 5) && (month <= 7)) {
+  return 'summer';
+}
+if ((month >= 8) && (month <= 10)) {
+  return 'fall';
+}
+  
+
+}
+
+
+/*const fakeDate = {
+  toString() {
+      return Date.prototype.toString.call(new Date());
+  },
+  [Symbol.toStringTag]: 'Date'
+};
+
+Object.setPrototypeOf(fakeDate, Object.getPrototypeOf(new Date()));
+
+getSeason(fakeDate);
+
+function getSeason(date) {
+
+  if(!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if ((Object.prototype.toString.call(date) === '[object Date]') && (!(isNaN(date.getTime())))) {
+  
+console.log(date.getTime());
+  }
+
+else {
+  
+throw new Error ('Invalid date!');
+
+}
+
+}*/
