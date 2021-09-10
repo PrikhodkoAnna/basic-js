14680a219d72a0a3d0d1982ccb24470b76c98969
PrikhodkoAnna <prikhodkoannav@gmail.com>
2021-09-10 12:23:18 +0300

    feat: add solution of transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,59 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error ("'arr' parameter must be an instance of the Array!");
+  }
+ 
+  let newArr = arr.filter(function(f){return f;})
+  
+  newArr.map(function check (elem, i, array) {
+
+      switch(elem) {
+      case '--discard-next': {
+        if ((array[i + 1]) && (typeof(array[i + 1]) === 'number')) {
+        newArr.splice(i, 2);
+        if ((newArr[i] === '--discard-prev') || (newArr[i] === '--double-prev')) {
+          newArr.splice(i, 1);
+      }
+        }
+        else {
+          newArr.splice(i, 1);
+        }
+        break;
+      }
+      case '--discard-prev': {
+        if ((array[i - 1]) && (typeof(array[i - 1]) === 'number')) {
+        newArr.splice(i - 1, 2);
+          }
+        else {
+        newArr.splice(i, 1);
+          }
+          break;
+      }
+      case '--double-next': {
+        if ((array[i + 1]) && (typeof(array[i + 1]) === 'number')) {
+        newArr.splice(i, 1, array[i + 1]);
+        }
+        else {
+          newArr.splice(i, 1);
+        }
+        break;
+      }
+      case '--double-prev':  {
+       if ((array[i - 1]) && (typeof(array[i - 1]) === 'number')) {
+        newArr.splice(i, 1, array[i - 1]);
+        }
+        else {
+          newArr.splice(i, 1);
+        }
+        break;
+      }
+    }
+  } )
+ 
+      console.log (arr);
+  return (newArr);
+  }
+
